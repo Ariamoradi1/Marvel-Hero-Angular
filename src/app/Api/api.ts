b93184c2b2bf74e1dcc7b2177c5692bfa3f36dff
Aria <ariaman2002@gmail.com>
2023-07-13 13:12:46 +0330

    Organizing the project structure

---
+++ src/app/Api/api.ts
@@ -0,0 +1,9 @@
+export const baseUrl : string = `https://gateway.marvel.com:443/v1/public/characters?`
+export const limit : string = 'limit=12&'
+export const timeSamp : string = 'ts=1&'
+export const apiKey : string = 'apikey=72befce71d3585a9c9534f88d3bb8625&'
+export const hashUrl : string = 'hash=f264f667c7beafdae1e0b90ada06788f'
+
+//comics data
+export const comicBaseUrl : string = `https://gateway.marvel.com:443/v1/public/series?`
+export const comicLimit : string = 'limit=10&'
